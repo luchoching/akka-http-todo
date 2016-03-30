cc80fc92deb003c11426b68932c707d2314d36e4
Lu Cho-Ching <choching.work@gmail.com>
2016-03-30 16:34:15 +0800

    Example: spray-json: Providing JsonFormats for other Types

--- src/main/scala/lu/todoapp/Main.scala
+++ src/main/scala/lu/todoapp/Main.scala
@@ -4,13 +4,34 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.stream.ActorMaterializer
-import spray.json.DefaultJsonProtocol
+import spray.json.{DefaultJsonProtocol, DeserializationException, JsNumber, JsObject, JsString, JsValue, RootJsonFormat}
 
 final case class Item(name: String, id: Long)
+final case class Order(items: List[Item])
+final case class Color(val name: String, val red: Int, val green: Int, val blue: Int)
+
 
 // collect our json format instances into a support trait
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val itemFormat = jsonFormat2(Item)
+  implicit val orderFormat = jsonFormat1(Order)
+
+  implicit object ColorJsonFormat extends RootJsonFormat[Color] {
+    def write(c: Color) = JsObject(
+      "name" -> JsString(c.name),
+      "red" -> JsNumber(c.red),
+      "green" -> JsNumber(c.green),
+      "blue" -> JsNumber(c.blue)
+    )
+
+    def read(value: JsValue) = {
+      value.asJsObject.getFields("2", "red", "green", "blue") match {
+        case Seq(JsString(name), JsNumber(red), JsNumber(green), JsNumber(blue)) =>
+          Color(name, red.toInt, green.toInt, blue.toInt)
+        case _ => throw new DeserializationException("Color expected")
+      }
+    }
+  }
 }
 
 object Main extends App with JsonSupport {
@@ -51,10 +72,28 @@ object Main extends App with JsonSupport {
           Item("thing", 42)
         }
       }
+    } ~
+    post {
+      entity(as[Order]) { order =>
+        val itemsCount = order.items.size
+        val itemNames = order.items.map(_.name).mkString(", ")
+        complete(s"Ordered $itemsCount items: $itemNames")
+
+      }
     }
 
+    val route4 =
+      pathSingleSlash {
+        post {
+          entity(as[Color]) { color =>
+            val colorName = color.name
+            complete(s"Color name: $colorName")
+          }
+        }
+      }
 
-  val bindingFuture = Http().bindAndHandle(route3,"localhost", 5678)
+
+  val bindingFuture = Http().bindAndHandle(route4,"localhost", 5678)
   println("""Server online at http://localhost:5678""")
 
 }
